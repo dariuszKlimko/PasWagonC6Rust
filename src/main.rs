aa7af7af4d9dcfc6c3acd8b938d481c7f1838c3e
dariuszKlimko <dariuszklimkoportfolio@gmail.com>
2022-08-28 23:14:07 +0200

    'JSON'

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
 #[macro_use] extern crate rocket;
-use rand::Rng;
 
-// Here is 6 warnings 4 "snake case name" and 2 "unused variable".
+use rand::Rng;
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+
+// Here is 8 warnings 6 "snake case name" and 2 "unused variable".
 // I do not change snake case because in Technical Requirements is used camel case.
 // Also I received 2 unused parameters but customer want to them here
 
+#[derive(Serialize)]
+struct Fuel {
+    fuelUsage: f32,
+}
+
 #[get("/calculateDieselUsageForDistance?<distance>&<yearOfProduction>&<fuelUsagePer100KM>")]
-fn calculatedieseldsagefordistance(distance: &str, yearOfProduction: &str, fuelUsagePer100KM: &str) -> String {
-    let dist: f32 = distance.parse().unwrap();
-    let year: f32 = yearOfProduction.parse().unwrap();
-    let fuel: f32 = fuelUsagePer100KM.parse().unwrap();
-    let fuelUsage: f32 = fuel*dist/100.0;
-    format!("{{fuelUsage: {}}}",fuelUsage)
+fn calculatedieseldsagefordistance(distance: &str, yearOfProduction: &str, fuelUsagePer100KM: &str) -> Json<Fuel> {
+    let distf32: f32 = distance.parse().unwrap();
+    let yearf32: f32 = yearOfProduction.parse().unwrap();
+    let fuelf32: f32 = fuelUsagePer100KM.parse().unwrap();
+    let fuelusage: f32 = fuelf32*distf32/100.0;
+    let fuel = Fuel {
+        fuelUsage: fuelusage,
+    };
+    Json(fuel)
+}
+
+#[derive(Serialize)]
+struct Fail {
+    failProbability: String,
 }
 
 #[get("/probabilityOfUnitInjectorFail?<VIN>")]
-fn probabilityofunitinjectorfail(VIN: &str) -> String {
+fn probabilityofunitinjectorfail(VIN: &str) -> Json<Fail>{
     let mut rng = rand::thread_rng();
-    let failprobability: u8 = rng.gen_range(0..101);
-    format!("{{failProbability: {}%}}",failprobability)
+    let failprobability = rng.gen_range(0..101);
+    let failStr = failprobability.to_string();
+    let fail = Fail {
+        failProbability: format!("{}%",failStr),
+    };
+    Json(fail)
 }
 
 #[launch]
